416ecf5551dc3d72260e9023da4334cc50d5f6d1
guo zheng mao <xiaoqguo@126.com>
2017-09-25 17:58:16 +0800

    Add router

--- angular2/src/app/app.component.ts
+++ angular2/src/app/app.component.ts
@@ -1,28 +1,17 @@
-import { Component,OnInit } from '@angular/core';
-import { Hero} from "./hero";
-import {HeroService} from "./hero.service";
+import {Component} from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-   providers: [HeroService]
+  selector:"my-app",
+  template:`
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/heroes">Heroes</a>
+      <a routerLink="/dashboard">Dashboard</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes ';
-  heroes:Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService:HeroService){}
-  getHeroes():void{
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit():void{
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
+export class AppComponent{
+   title = 'Tour of Heroes';
 }
--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -1,19 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from './heros.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.components';
+import { HeroService } from "./hero.service";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'heroes',
+        component:HeroesComponent
+      },
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
